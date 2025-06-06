19f6df994421af66bc3fd600b6b775308eafa995
himanshu goyal <hiteshgoyal943gmail.com>
2025-06-06 15:20:46 +0530

    fix table

--- index.js
+++ index.js
@@ -7,27 +7,12 @@ const sqlite3 = require("sqlite3").verbose();
 const sqliteInstance = new sqlite3.Database("session_storage.db");
 
 app.listen(port, () => {
-  // CREATE TABLE IF NOT EXISTS token_store (
-  //   company_id TEXT,
-  //   application_id TEXT,
-  //   token TEXT,
-  //   PRIMARY KEY (company_id, application_id)
-  // )
-
-  sqlite3.run(
+  sqliteInstance.run(
     `CREATE TABLE IF NOT EXISTS token_store (
       company_id TEXT,
       token TEXT,
       key TEXT,
-    )`,
-    (err) => {
-      if (err) {
-        console.error("Error creating table:", err.message);
-      } else {
-        console.log("Token store table is ready.");
-      }
-    }
+    )`
   );
-
   console.log(`Example app listening at http://localhost:${port}`);
 });
